e8ebc53702fbf76fbb3b95cd38fd9aab9f0a04e4
Jaylen <jaywill7@uw.edu>
2017-12-06 08:10:44 -0800

    Edited HTML page for presentation

--- slackers/ui.R
+++ slackers/ui.R
@@ -7,7 +7,7 @@ shinyUI(navbarPage(theme = shinytheme("cerulean"), "Terrorism in the United Stat
      tabPanel("Where it happens",
         sidebarLayout(
           sidebarPanel(
-            textInput("location", label = h3("Which Location would you like to explore?"), value = "Washington"),
+            textInput("location", label = h3("Which location would you like to explore?"), value = "Washington"),
             
             hr(),
             
@@ -23,7 +23,7 @@ shinyUI(navbarPage(theme = shinytheme("cerulean"), "Terrorism in the United Stat
      tabPanel("Amount of Attacks",
               sidebarLayout(
                 sidebarPanel(
-                  radioButtons("typeAttack", label = h3("Choose a type of terrorist attack"), selected = "All types",
+                  radioButtons("typeAttack", label = h3("Choose a Type of Terrorist Attack"), selected = "All types",
                                choices = list("All types" = "All types", 
                                               "Armed Assault" = "Armed Assault", 
                                               "Explosives" = "Explosives",
@@ -39,7 +39,7 @@ shinyUI(navbarPage(theme = shinytheme("cerulean"), "Terrorism in the United Stat
      tabPanel("Type Of Attack",
               sidebarLayout(
                 sidebarPanel(
-                  checkboxGroupInput("typeOfAttack", label = h3("Select Type"),
+                  checkboxGroupInput("typeOfAttack", label = h3("Select Type of Attack"),
                                      choices = list("Armed Assault" = 'Armed Assault', "Explosives" = 'Explosives', "Facility/Infrastructure attack" = 'building attack', "Unarmed Assault" = 'Unarmed Assault'), selected = c("Armed Assault","Explosives", "Facility/Infrastructure attack", "Unarmed Assault"))                  
                 ),
                 mainPanel(
@@ -49,17 +49,6 @@ shinyUI(navbarPage(theme = shinytheme("cerulean"), "Terrorism in the United Stat
               
               ),
      tabPanel("Attacks by Nationality",
-               # sidebarLayout(
-               # sidebarPanel(
-               #   textInput("location", label = h3("Which Location would you like to explore?"), value = "Washington"),
-                 
-               #   hr(),
-                  
-              #    sliderInput("zoom", label = h3("Zoom"), min = 3, 
-             #                 max = 20, value = 6)
-               # ),
-                
-                # Main panel displaying the plot
                 mainPanel(
                   plotOutput("nationality")
                 )
